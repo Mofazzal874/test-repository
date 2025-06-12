359fbb46a4496e3d3c140d9af6db3a369852dd41
Mofazzal <mofazzalhossen874@gmail.com>
2025-06-13 01:47:44 +0600

    new space is added and numbers added in index.js

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ if(name === 'Arman') {
 } else {
     console.log('Hello, stranger!');
 }
+
+
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.forEach((number) => {
+    console.log(`Number: ${number}`);
+});
